4f0a31874221f919984ce45a2984d85eef40cf17
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2022-09-11 19:40:02 -0300

    Modificado Home

---
+++ js/effects.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    const tags = $(".tag");
+    const homeTag = tags[0];
+    $(homeTag).addClass("visible");
+});
+
+
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+    //console.log(pageBottom);
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
